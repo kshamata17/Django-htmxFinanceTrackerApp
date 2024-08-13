c5c6ebfcc4dd9f2c6db3c967510054d332111a0c
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-13 18:00:36 +0545

    models registered in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('transaction_user', 'transaction_category', 'transaction_type', 'transaction_amount', 'transaction_date')
