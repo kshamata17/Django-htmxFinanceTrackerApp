6da5d89cab0c3cfd703f1755c574ef37748702d3
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-13 17:59:07 +0545

    transaction list passed to template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
+from .filters import TransactionFilter
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    user = request.user
+    context = {'user': user}
+    return render(request, 'main/home.html', context)
+
+@login_required
+def transactions(request):
+    transaction_filter = TransactionFilter(
+        request.GET, queryset=Transaction.objects.filter(transaction_user=request.user)
+     )
+    transactions = transaction_filter.qs
+    context = {'filter': transaction_filter}
+    return render(request, 'main/transactions.html', context)
