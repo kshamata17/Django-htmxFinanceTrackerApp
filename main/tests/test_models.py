f4bbd65bbfa8d5b0469ac54f57ba5353545d2eb0
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-15 18:02:55 +0545

    functions created for test_models

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -4,7 +4,7 @@ from main.models import Transaction, Category
 
 @pytest.mark.django_db
 def test_queryset_get_income_method(transactions):
-    qs = Transaction.objects.get_income()
+    qs = Transaction.objects.get_incomes()
     assert qs.count() > 0
     assert all(
         [transaction.transaction_type == 'income' for transaction in qs]
@@ -12,8 +12,17 @@ def test_queryset_get_income_method(transactions):
 
 @pytest.mark.django_db
 def test_queryset_get_expense_method(transactions):
-    qs = Transaction.objects.get_expense()
+    qs = Transaction.objects.get_expenses()
     assert qs.count() > 0
     assert all(
         [transaction.transaction_type == 'expense' for transaction in qs]
     )
+@pytest.mark.django_db
+def test_queryset_get_total_income_method(transactions):
+    total_income = Transaction.objects.get_total_incomes()
+    assert total_income == sum(t.transaction_amount for t in transactions if t.transaction_type == 'income')
+
+@pytest.mark.django_db
+def test_queryset_get_total_expenses_method(transactions):
+    total_expenses = Transaction.objects.get_total_expenses()
+    assert total_expenses == sum(t.transaction_amount for t in transactions if t.transaction_type == 'expense')
