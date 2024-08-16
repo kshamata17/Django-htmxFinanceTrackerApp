7cb157282a064b3fb369a056197eac95f2b2d9ab
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-16 18:00:48 +0545

    create transaction url added

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='main-home'),
     path('transactions/', views.transactions_list, name='main-transactions'),
+    path('transactions/create', views.create_transaction, name='create-transaction'),
 ]
