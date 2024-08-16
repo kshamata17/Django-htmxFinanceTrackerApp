9fd66aede73e77a4694ed2a814a9cce42ee80dc9
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-16 17:45:17 +0545

    files added in gitignore

--- main/tests/conftest.py
+++ main/tests/conftest.py
@@ -1,7 +1,11 @@
 import pytest
 from main.factories import *
 
-
 @pytest.fixture
 def transactions():
     return TransactionFactory.create_batch(20)
+
+@pytest.fixture
+def user_transactions():
+    user = UserFactory()
+    return TransactionFactory.create_batch(20, transaction_user=user)
