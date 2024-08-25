52e3d8e678c2092c53f5bb0f9d26269b01eb5b07
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-25 17:39:27 +0545

    urls for login and register added

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='main-home'),
     path('transactions/', views.transactions_list, name='main-transactions'),
     path('transactions/create', views.create_transaction, name='create-transaction'),
+    path('transactions/login', views.login, name='login'),
+    path('transactions/register', views.register, name='register'),
 ]
