5749e07cbd8cb5a9187173b62da4f6556e693363
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-27 20:29:40 +0545

    urls for new pages added

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('', views.home, name='main-home'),
     path('transactions/', views.transactions_list, name='main-transactions'),
     path('transactions/create', views.create_transaction, name='create-transaction'),
-    path('transactions/login', views.login, name='login'),
-    path('transactions/register', views.register, name='register'),
+    path('transactions/login', views.user_login, name='login'),
+    path('transactions/register', views.user_register, name='register'),
+
+    path('transactions/<int:pk>/update', views.update_transaction, name='update-transaction'),
+    path('transactions/<int:pk>/delete', views.delete_transaction, name='delete-transaction'),
+
+    path('get-transactions/', views.get_transactions, name='get-transactions'),
 ]
