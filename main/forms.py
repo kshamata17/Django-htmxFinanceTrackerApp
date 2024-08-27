6f39215a57f5b6e01610361bc84c4e56fd895897
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-27 20:25:14 +0545

    userregistrationform created

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,43 @@
 from django import forms
 from .models import Transaction, Category
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True, 
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your email', 
+            'class': 'form-control'
+        })
+    )
+
+    username = forms.CharField(
+        required=True, 
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your username', 
+            'class': 'form-control'
+        })
+    )
+
+    password1 = forms.CharField(
+        required=True, 
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter your password', 
+            'class': 'form-control'
+        })
+    )
+
+    password2 = forms.CharField(
+        required=True, 
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Confirm your password', 
+            'class': 'form-control'
+        })
+    )
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 class TransactionForm(forms.ModelForm):
     transaction_category = forms.ModelChoiceField(
