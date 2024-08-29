96f90e858e20fb6bd6529f37b39157acdc1bd974
Kshamata Pariyar <Kshamata12@gmail.com>
2024-08-29 11:33:57 +0545

    TransactionResource class added

---
+++ main/resources.py
@@ -0,0 +1,18 @@
+from import_export import resources, fields
+from .models import Transaction, Category
+from import_export.widgets import ForeignKeyWidget
+
+class TransactionResource(resources.ModelResource):
+    category = fields.Field(
+        column_name='category',
+        attribute='transaction_category',
+        widget=ForeignKeyWidget(Category, 'category_name')
+    )
+    class Meta:
+        model = Transaction
+        fields = (
+            'transaction_category', 
+            'transaction_type', 
+            'transaction_amount', 
+            'transaction_date',
+            )
