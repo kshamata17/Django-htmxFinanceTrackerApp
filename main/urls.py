843a54a942bfe890e4ad58aaf70f5f5b9637f1c0
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-02 18:27:43 +0545

    login and register url removed

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('', views.home, name='main-home'),
     path('transactions/', views.transactions_list, name='main-transactions'),
     path('transactions/create', views.create_transaction, name='create-transaction'),
-    path('transactions/login', views.user_login, name='login'),
-    path('transactions/register', views.user_register, name='register'),
 
     path('transactions/<int:pk>/update', views.update_transaction, name='update-transaction'),
     path('transactions/<int:pk>/delete', views.delete_transaction, name='delete-transaction'),
