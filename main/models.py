9f9512d32e5a8d57c80231375a28f3da560798ea
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-03 17:47:24 +0545

    User model removed

--- main/models.py
+++ main/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from .managers import TransactionQuerySet
+from user.models import User
 
 # Create your models here.
-class User(AbstractUser):
-    pass
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
