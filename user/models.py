f8b0fe8d19e4058db3aac22eb21b42e97166fdfe
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-03 17:49:35 +0545

    Profile model created

--- user/models.py
+++ user/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    birth_date = models.CharField(max_length=255, blank=True, null=True)
+    def __str__(self):
+        return self.user.username
