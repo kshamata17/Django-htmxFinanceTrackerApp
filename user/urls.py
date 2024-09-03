0c155ee35fce7864f5110d56dd65e425b42433b9
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-03 17:51:07 +0545

    profile url added

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.userLogin, name='user-login'),
     path('register/', views.userRegister, name='user-register'),
+
+    path('profile/', views.userProfile, name='user-profile'),
+
 ]
