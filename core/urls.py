22a6083af79cf579ee8a609894d515188098a7e1
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-03 17:46:47 +0545

    url added for logout

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), #main app url
     path('user/', include('user.urls')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html', next_page='user-login'), name='user-logout'), #logout
 ]
