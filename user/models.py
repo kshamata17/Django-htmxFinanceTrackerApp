7ab1f19924e1542e85c9b7f3c04930a9ae496598
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-04 17:39:39 +0545

    profile_image field added

--- user/models.py
+++ user/models.py
@@ -7,10 +7,12 @@ class User(AbstractUser):
     pass
 
 class Profile(models.Model):
+    # profile_id = models.AutoField(primary_key=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     address = models.CharField(max_length=255, blank=True, null=True)
     birth_date = models.CharField(max_length=255, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     def __str__(self):
         return self.user.username
