5d7b1f43a014f5dc624ee21e00bfb9c734dc5a97
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-10 16:56:10 +0545

    widgets added to login form and register form

--- user/forms.py
+++ user/forms.py
@@ -1,15 +1,38 @@
 from django import forms
 from django.contrib.auth import get_user_model
+
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from user.models import Profile
 
 class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your email',
+    }))
 
-    email = forms.EmailField()
+    class Meta: 
+        model = User
+        fields = ['username', 'email', 'password1', 'password2',]
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your username',
+            }),
+        }
 
-    class Meta:
-        model = get_user_model()
-        fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': 'Enter your password'
+        })
+
+        self.fields['password2'].widget.attrs.update({  
+            'class': 'form-control',
+            'placeholder': 'Confirm your password'
+        })
 
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -38,3 +61,30 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = "__all__"
+        widgets = {
+            'user': forms.Select(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your username',   
+            }),
+            'bio': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Tell us about you',
+                'rows': '4',
+            }),
+            'profile_image': forms.FileInput(attrs={
+                'class': 'form-control',
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Where are you from?',
+            }),
+            'balance': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': '0.00',
+            }),
+            'birth_date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            }),
+
+        }
